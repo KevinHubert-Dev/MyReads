6a00d31907e6b558955f52b0507bb02e4bbb0f63
Kevin Hubert <>
2018-06-11 23:54:14 +0200

    feat: Add BookShelter as React-Component
    Add new component called BookShelter which is responsible to render a Shelter of Books (kind of an category/status).
    A BookShelter contains a title to descripe the status of the books inside of the Shelter e.g. "Want to read" and moreover the BookShelter get the Books passed as a prop to show them inside of the Shelter using the Book-ReactComponent.
    Currently the BookShelter-Component could be a functional-component, but it will be extended later so i've already made it a component extend the React.Component-Class.

---
+++ src/Components/BookShelter.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Book from './Book'
+
+
+class BookShelf extends Component {
+
+  render() {
+    let { title, books } = this.props
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {
+              books.map(book => (
+                <Book
+                  title={book.title}
+                  authors={book.authors}
+                  imageUrl={book.imageUrl}
+                />
+              ))
+            }
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+BookShelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired
+}
+
+export default BookShelf
